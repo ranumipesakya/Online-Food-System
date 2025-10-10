5618b1632a771f218cd46fc856f5424e50fd7334
ranumipesakya <154657664+ranumipesakya@users.noreply.github.com>
2025-10-10 13:06:53 +0530

    first commit

---
+++ frontend/src/componenets/FoodItem/FoodItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FoodItem.css'
+
+const FoodItem = ({id,name,price,description,image}) => {
+  return (
+    <div className='food-item'>
+      div.food-item-img-cont
+
+    </div>
+  )
+}
+
+export default FoodItem
---
+++ frontend/src/componenets/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2> Order Your Favourite food here</h2>
+         <p>  Choose from a diverse meu featuring a delectable array of dishes crafted with the finest ingrediants and elevate your dining experience, one delicious meal at a time </p>
+      <button> View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
